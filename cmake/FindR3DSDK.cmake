96b43810c2b2f1274adcd8a11580c7d7868f234b
Will Rosecrans <wrosecrans@gmail.com>
2019-09-14 12:55:42 -0700

    Rename variables to be more ocnsistent with the name of the CMake file. Could probably have renamed the CMake file, this was suggested in a discussion on oiio on an equivalent findR3D.
    Variable names with a 3 in them are ugly, but I blame the camera company
    for the name.

--- cmake/FindR3DSDK.cmake
+++ cmake/FindR3DSDK.cmake
@@ -2,25 +2,25 @@
 #
 # Inputs:
 #
-#  Set RED_ROOT Environment variable to root of the SDK
+#  Set R3DSDK_ROOT Environment variable to root of the SDK
 #    Otherwise it will try to guess some plausible paths.
-#  Set RED_LIB_NAME if it is guessing the wrong library and causing linker errors.
+#  Set R3D_LIB_NAME if it is guessing the wrong library and causing linker errors.
 #
 # Outputs - Once done, this will define:
 #
-#  RED_FOUND - system has R3D SDK
-#  RED_INCLUDE_DIR - the include directory
-#  RED_LIBRARIES - link these at build time.
-#  RED_RUNTIME_LIBRARIES - Use this path (or copy the contents to a path)
+#  R3DSDK_FOUND - system has R3D SDK
+#  R3DSDK_INCLUDE_DIR - the include directory
+#  R3DSDK_LIBRARIES - link these at build time.
+#  R3DSDK_RUNTIME_LIBRARIES - Use this path (or copy the contents to a path)
 #          for R3DSDK::InitializeSdk() at runtime.
 
 include (FindPackageHandleStandardArgs)
 
 # Assuming only C++11 and 64 bit support is needed with new-ish compiler.
-# Set RED_LIB_NAME before invoking this file to override.
-if (NOT DEFINED RED_LIB_NAME)
+# Set R3D_LIB_NAME before invoking this file to override.
+if (NOT DEFINED R3D_LIB_NAME)
     if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
-        set(RED_LIB_NAME R3DSDKPIC-cpp11)
+        set(R3D_LIB_NAME R3DSDKPIC-cpp11)
     else()
         # It's rumored that there was once someone who completely understood
         # the differences between the various MSVCRT MT, MD, MDd, etc. options.
@@ -32,18 +32,18 @@ if (NOT DEFINED RED_LIB_NAME)
 
         # message(${CMAKE_BUILD_TYPE})
         if(CMAKE_BUILD_TYPE MATCHES Debug)
-            set(RED_LIB_NAME R3DSDK-2017MDd)
+            set(R3D_LIB_NAME R3DSDK-2017MDd)
         else()
-            set(RED_LIB_NAME R3DSDK-2017MD)
+            set(R3D_LIB_NAME R3DSDK-2017MD)
         endif()
     endif()
 endif()
 
 
 
-if(DEFINED ENV{RED_ROOT})
-    set(RED_ROOT $ENV{RED_ROOT})
-    # message("Using R3D SDK Root: " ${RED_ROOT})
+if(DEFINED ENV{R3DSDK_ROOT})
+    set(R3DSDK_ROOT $ENV{R3DSDK_ROOT})
+    # message("Using R3D SDK Root: " ${R3DSDK_ROOT})
 else()
     # message("Looking for R3D SDK")
 
@@ -51,42 +51,42 @@ else()
     # So try some plausible locations and hope for the best.
     # Use a glob to try and avoid dealing with exact SDK version numbers.
 
-    file(GLOB RED_SDK_PATHS "/opt/R3DSDKv*"
+    file(GLOB R3DSDK_PATHS "/opt/R3DSDKv*"
         "C:/R3DSDKv*"
         ${CMAKE_CURRENT_SOURCE_DIR}/../R3DSDKv*
         ${CMAKE_CURRENT_SOURCE_DIR}/../../R3DSDKv*
         ${CMAKE_CURRENT_SOURCE_DIR}/../../../R3DSDKv*
         )
-    message("Looking in " ${RED_SDK_PATHS})
-    find_path(RED_ROOT Include/R3DSDK.h
-              PATHS ${RED_SDK_PATHS}
+    message("Looking in " ${R3DSDK_PATHS})
+    find_path(R3DSDK_ROOT Include/R3DSDK.h
+              PATHS ${R3DSDK_PATHS}
              )
 endif()
 
-set(RED_INCLUDE_DIR ${RED_ROOT}/Include)
-find_library(RED_LIBRARY NAMES ${RED_LIB_NAME}
-             PATHS ${RED_ROOT}/Lib/linux64/
-                   ${RED_ROOT}/Lib/win64/
+set(R3DSDK_INCLUDE_DIR ${R3DSDK_ROOT}/Include)
+find_library(R3D_LIBRARY NAMES ${R3D_LIB_NAME}
+             PATHS ${R3DSDK_ROOT}/Lib/linux64/
+                   ${R3DSDK_ROOT}/Lib/win64/
              NO_DEFAULT_PATH
             )
                    
 # handle the QUIETLY and REQUIRED arguments and set RED_FOUND to TRUE if
 # all listed variables are TRUE
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(RED DEFAULT_MSG RED_LIBRARY RED_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(RED DEFAULT_MSG R3D_LIBRARY R3DSDK_INCLUDE_DIR)
 
 
 # TODO : Ask somebody with a Mac to add support.
-if(RED_FOUND)
+if(R3DSDK_FOUND)
     if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
-        set(RED_LIBRARIES ${RED_LIBRARY} dl)
-        set(RED_RUNTIME_LIBRARIES ${RED_ROOT}/Redistributable/linux)
+        set(R3D_LIBRARIES ${RED_LIBRARY} dl)
+        set(R3DSDK_RUNTIME_LIBRARIES ${RED_ROOT}/Redistributable/linux)
     else()
-        set(RED_LIBRARIES ${RED_LIBRARY})
-        set(RED_RUNTIME_LIBRARIES ${RED_ROOT}/Redistributable/win)
+        set(R3D_LIBRARIES ${RED_LIBRARY})
+        set(R3DSDK_RUNTIME_LIBRARIES ${R3DSDK_ROOT}/Redistributable/win)
     endif()
 endif()
 
 # message("R3D SDK: " ${RED_INCLUDE_DIR} "  " ${RED_LIBRARIES} "  " ${RED_RUNTIME_LIBRARIES})
 
-mark_as_advanced(RED_LIBRARY RED_INCLUDE_DIR)
+mark_as_advanced(R3D_LIBRARY R3DSDK_INCLUDE_DIR)
 
